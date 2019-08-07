f6f6e4658269ea03c25fd6fa51b4a29b3c944af2
James Rory Hanley <hanley.doth@outlook.com>
2019-08-07 16:05:10 -0700

    swag

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { LandingComponent } from './components/landing/landing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LandingComponent},
+  {path: 'experience', component: ExperienceComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { ExperienceComponent } from './components/experience/experience.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    LandingComponent,
+    ExperienceComponent
   ],
   imports: [
     BrowserModule,
