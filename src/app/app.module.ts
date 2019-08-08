cfe0c6d94ee014ff4ebf840e5b7fbfbd9e823b63
James Rory Hanley <hanley.doth@outlook.com>
2019-08-08 12:41:07 -0700

    Add different styling, fix sidebar references/names

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { LandingComponent } from './components/landing/landing.component';
 import { ExperienceComponent } from './components/experience/experience.component';
+import { AboutComponent } from './components/about/about.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
     LandingComponent,
-    ExperienceComponent
+    ExperienceComponent,
+    AboutComponent,
+    PortfolioComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
